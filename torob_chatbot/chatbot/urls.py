a6e38fb8e2c812a8647fcc5eb02e10833ed6e461
amirerfantim <amirerfantim@gmail.com>
2023-11-11 20:37:39 +0330

    Update urls.py
    add some endpoints

---
+++ torob_chatbot/chatbot/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import register, home, login_view
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('', home, name='home')
+]
