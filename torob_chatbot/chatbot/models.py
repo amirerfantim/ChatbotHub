708b9ed07242550fe9408b48879978c3f8274bc6
amirerfantim <amirerfantim@gmail.com>
2023-11-11 20:33:10 +0330

    Update models.py
    update CustomUser

--- torob_chatbot/chatbot/models.py
+++ torob_chatbot/chatbot/models.py
@@ -1,20 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    user_type_choices = [
-        ('normal', 'Normal User'),
-        ('chatbot_builder', 'Chatbot Builder'),
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('regular', 'Regular User'),
+        ('chatbot_creator', 'Chatbot Creator'),
         ('admin', 'Admin'),
     ]
-    user_type = models.CharField(max_length=15, choices=user_type_choices, default='normal')
-    username = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='regular')
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name='customuser_set',  # Change this to a name that makes sense for your project
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='customuser_set',  # Change this to a name that makes sense for your project
+        related_query_name='user',
+    )
 
 
 class Chatbot(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
     is_active = models.BooleanField(default=True)
@@ -29,19 +45,19 @@ class ChatbotContent(models.Model):
 
 class Conversation(models.Model):
     chatbot = models.ForeignKey(Chatbot, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class UserRating(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     like = models.BooleanField(default=False)
     dislike = models.BooleanField(default=False)
