3a5a8e02da2596cda038e507ad47c9a89998e1dd
amirerfantim <amirerfantim@gmail.com>
2023-11-18 18:21:53 +0330

    Update tests.py
    add more tests

--- torob_chatbot/chatbot/tests.py
+++ torob_chatbot/chatbot/tests.py
@@ -36,7 +36,6 @@ class ChatbotViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
 
-
 class ChatDetailsViewTestCase(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
@@ -55,4 +54,33 @@ class ChatDetailsViewTestCase(TestCase):
         response = self.client.get(f'/chat-details/{self.conversation.id}/')
         self.assertEqual(response.status_code, 302)
 
+    def test_start_conversation_view(self):
+        self.client.login(username='test@example.com', password='testpassword')
 
+        response = self.client.post('/start-conversation/', {'chatbot_id': self.chatbot.id})
+        self.assertEqual(response.status_code, 302)
+
+
+class SendMessageViewTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword',
+            email='test@example.com'
+        )
+
+        self.chatbot = Chatbot.objects.create(user=self.user, name='Test Chatbot', description='Test Description')
+
+        self.conversation = Conversation.objects.create(chatbot=self.chatbot, user=self.user)
+
+    def test_send_message_view(self):
+        self.client.force_login(self.user)
+
+        initial_message_count = Message.objects.filter(conversation=self.conversation).count()
+
+        response = self.client.post(reverse('send_message', args=[self.conversation.id]), {'content': 'Test message'})
+        self.assertEqual(response.status_code, 302)
+
+        updated_message_count = Message.objects.filter(conversation=self.conversation).count()
+
+        self.assertEqual(updated_message_count, initial_message_count + 2)
