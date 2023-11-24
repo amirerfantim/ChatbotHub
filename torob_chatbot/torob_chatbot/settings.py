a6bbef6d005b5d61c1b33ec6d19877266abff49c
amirerfantim <amirerfantim@gmail.com>
2023-11-24 19:18:01 +0330

    Update settings.py
    use environment variable

--- torob_chatbot/torob_chatbot/settings.py
+++ torob_chatbot/torob_chatbot/settings.py
@@ -84,30 +84,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'torob_chatbot.wsgi.application'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ['DB_NAME'],
-    #     'USER': os.environ['DB_USER'],
-    #     'PASSWORD': os.environ['DB_PASSWORD'],
-    #     'HOST': os.environ['DB_HOST'],
-    #     'PORT': os.environ['DB_PORT'],
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'pgvectorcontainer.torob-bootcamp-1402-teimouri.svc',
-        'PORT': '5432',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'test',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
 }
 
 
