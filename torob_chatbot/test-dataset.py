07760ff497d29b213dca8b397e59d9cf86fd0285
amirerfantim <amirerfantim@gmail.com>
2023-11-25 14:42:12 +0330

    Update services.py & test-dataset.py

--- torob_chatbot/test-dataset.py
+++ torob_chatbot/test-dataset.py
@@ -1,3 +1,45 @@
-from chatbot.services import test_dataset
+import json
+import time
 
-test_dataset(7, "data/data2.jsonl")
+from pgvector.django import MaxInnerProduct
+
+from chatbot.models import Chatbot, ChatbotContent
+from chatbot.services import embedding
+
+
+def test_dataset(chatbot_id, jsonl_file_path):
+    chatbot = Chatbot.objects.get(id=chatbot_id)
+
+    with open(jsonl_file_path, "r", encoding="utf-8") as file:
+        i = 1
+        count = 0
+        for line in file:
+            data = json.loads(line)
+
+            doc_content = data.get("doc", "")
+            question_content = data.get("question", "")
+
+            user_message_embedding = embedding(question_content, max_retries=25, sleep=5)
+
+            chatbot_contents = ChatbotContent.objects.filter(chatbot=chatbot)
+
+            ordered_chatbot_contents = chatbot_contents.order_by(MaxInnerProduct('embedding', user_message_embedding))
+
+            most_relevant_content = ordered_chatbot_contents.first().content
+
+            print("******************\n")
+
+            if doc_content == most_relevant_content:
+                count += 1
+                print(str(i), " was success, success count so far:  ", str(count), "\n")
+            else:
+                print(str(i), " was fail, success count so far: ", str(count) + "\n")
+                print("retrieved doc: ", most_relevant_content, '\n')
+                print("original doc: ", doc_content, '\n')
+                print()
+            print("******************\n")
+            i += 1
+        print("similarity: ", count / i)
+
+
+test_dataset(15, "data/data.jsonl")
