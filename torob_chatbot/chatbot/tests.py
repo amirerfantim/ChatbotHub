0b72d2f0cba7177c0256ec0da45f1c2558d22786
amirerfantim <amirerfantim@gmail.com>
2023-11-26 23:01:08 +0330

    Add some tests to tests.py

--- torob_chatbot/chatbot/tests.py
+++ torob_chatbot/chatbot/tests.py
@@ -2,7 +2,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from .models import Chatbot, Conversation, Message
+from .models import Chatbot, Conversation, Message, ChatbotContent
 
 
 class ChatbotViewsTestCase(TestCase):
@@ -156,7 +156,6 @@ class StartConversationViewTests(TestCase):
 
 class ChatHistoryViewTest(TestCase):
     def setUp(self):
-        # Create a user for testing
         self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
         image_content = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x01\x00\x00\x00\x01\x00\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDAT\x08\xd7c`\x00\x00\x00\x02\x00\x01\x15\x00\x00\x00\x00IEND\xaeB`\x82'
         self.image = SimpleUploadedFile("test_image.png", image_content, content_type="image/png")
@@ -166,10 +165,8 @@ class ChatHistoryViewTest(TestCase):
         self.inactive_chatbot = Chatbot.objects.create(user=self.user, name='Inactive Chatbot', is_active=False,
                                                        bot_photo=self.image)
 
-        # Create conversations for the active chatbot
         Conversation.objects.create(chatbot=self.active_chatbot, user=self.user, title='Conversation 1')
         Conversation.objects.create(chatbot=self.active_chatbot, user=self.user, title='Conversation 2')
-
 
     def test_user_redirected_to_login_if_not_logged_in(self):
         response = self.client.get(reverse('chat_history'))
@@ -188,3 +185,83 @@ class ChatHistoryViewTest(TestCase):
         self.assertQuerysetEqual(response.context['conversations'], [])
 
 
+class LikeDislikeMessageViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="test_user", password="test_password")
+        self.client.login(username="test_user", password="test_password")
+        self.chatbot = Chatbot.objects.create(user=self.user)
+        self.content = ChatbotContent.objects.create(chatbot=self.chatbot)
+        self.conversation = Conversation.objects.create(chatbot=self.chatbot, user=self.user)
+        self.message = Message.objects.create(conversation=self.conversation)
+
+    def test_like_message(self):
+        url = reverse('like_dislike_message', args=[self.message.id, 'like'])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+
+        updated_message = Message.objects.get(pk=self.message.id)
+
+        self.assertEqual(updated_message.likes, 1)
+
+    def test_dislike_message(self):
+        url = reverse('like_dislike_message', args=[self.message.id, 'dislike'])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+
+        updated_message = Message.objects.get(pk=self.message.id)
+
+        self.assertEqual(updated_message.dislikes, 1)
+        self.assertNotEqual(updated_message.content, self.message.content)
+
+    def test_invalid_message_id(self):
+        url = reverse('like_dislike_message', args=[999, 'like'])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content, b'Invalid message ID')
+
+
+class SwitchContentViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="test_user", password="test_password")
+        self.client.login(username="test_user", password="test_password")
+        self.chatbot = Chatbot.objects.create(user=self.user)
+        self.content = ChatbotContent.objects.create(chatbot=self.chatbot)
+        self.conversation = Conversation.objects.create(chatbot=self.chatbot, user=self.user)
+        self.assistant_message = Message.objects.create(conversation=self.conversation, role='assistant', dislikes=1)
+        self.user_message = Message.objects.create(conversation=self.conversation, role='user')
+
+    def test_switch_content_show_original(self):
+        url = reverse('switch_content', args=[self.assistant_message.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        updated_message = Message.objects.get(pk=self.assistant_message.id)
+
+        self.assertTrue(updated_message.show_original)
+
+    def test_switch_content_hide_original(self):
+        self.assistant_message.show_original = True
+        self.assistant_message.save()
+
+        url = reverse('switch_content', args=[self.assistant_message.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        updated_message = Message.objects.get(pk=self.assistant_message.id)
+
+        self.assertFalse(updated_message.show_original)
+
+    def test_switch_content_user_message(self):
+        url = reverse('switch_content', args=[self.user_message.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        updated_user_message = Message.objects.get(pk=self.user_message.id)
+
+        self.assertIsNone(updated_user_message.show_original)
+
+    def test_switch_content_invalid_message_id(self):
+        url = reverse('switch_content', args=[999])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content, b'Invalid message ID')
