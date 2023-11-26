682430d48f0d795da5d7baf1506da028b345643f
amirerfantim <amirerfantim@gmail.com>
2023-11-26 22:53:23 +0330

    Add some tests.py

--- torob_chatbot/chatbot/tests.py
+++ torob_chatbot/chatbot/tests.py
@@ -1,3 +1,4 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -103,3 +104,87 @@ class SendMessageViewTestCase(TestCase):
         updated_message_count = Message.objects.filter(conversation=self.conversation).count()
 
         self.assertEqual(updated_message_count, initial_message_count + 2)
+
+
+class StartConversationViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+
+        image_content = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x01\x00\x00\x00\x01\x00\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDAT\x08\xd7c`\x00\x00\x00\x02\x00\x01\x15\x00\x00\x00\x00IEND\xaeB`\x82'
+        self.image = SimpleUploadedFile("test_image.png", image_content, content_type="image/png")
+
+        self.chatbot = Chatbot.objects.create(
+            user=self.user,
+            name='Test Chatbot',
+            description='Test Description',
+            custom_prompt='Test Prompt',
+            is_active=True,
+            bot_photo=self.image
+        )
+
+    def test_start_conversation_post(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.post(reverse('start_conversation'), {'chatbot_id': self.chatbot.id})
+
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(Conversation.objects.count(), 1)
+
+        conversation = Conversation.objects.first()
+        self.assertEqual(conversation.chatbot, self.chatbot)
+        self.assertEqual(conversation.user, self.user)
+
+        self.assertRedirects(response, reverse('chat_details', args=[conversation.id]))
+
+    def test_start_conversation_get(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.get(reverse('start_conversation'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertQuerysetEqual(response.context['chatbots'], Chatbot.objects.all(), transform=lambda x: x)
+
+    def test_start_conversation_unauthenticated(self):
+        response = self.client.post(reverse('start_conversation'), {'chatbot_id': self.chatbot.id})
+
+        self.assertEqual(response.status_code, 302)
+
+        self.assertRedirects(response, reverse('login') + '?next=' + reverse('start_conversation'))
+
+
+class ChatHistoryViewTest(TestCase):
+    def setUp(self):
+        # Create a user for testing
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        image_content = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x01\x00\x00\x00\x01\x00\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDAT\x08\xd7c`\x00\x00\x00\x02\x00\x01\x15\x00\x00\x00\x00IEND\xaeB`\x82'
+        self.image = SimpleUploadedFile("test_image.png", image_content, content_type="image/png")
+
+        self.active_chatbot = Chatbot.objects.create(user=self.user, name='Active Chatbot', is_active=True,
+                                                     bot_photo=self.image)
+        self.inactive_chatbot = Chatbot.objects.create(user=self.user, name='Inactive Chatbot', is_active=False,
+                                                       bot_photo=self.image)
+
+        # Create conversations for the active chatbot
+        Conversation.objects.create(chatbot=self.active_chatbot, user=self.user, title='Conversation 1')
+        Conversation.objects.create(chatbot=self.active_chatbot, user=self.user, title='Conversation 2')
+
+
+    def test_user_redirected_to_login_if_not_logged_in(self):
+        response = self.client.get(reverse('chat_history'))
+
+        self.assertRedirects(response, '/login/?next=/chat-history/', status_code=302, target_status_code=200)
+
+    def test_no_conversations_to_display(self):
+        user_without_conversations = get_user_model().objects.create_user(username='noconvuser', password='testpass')
+
+        self.client.login(username='noconvuser', password='testpass')
+
+        response = self.client.get(reverse('chat_history'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertQuerysetEqual(response.context['conversations'], [])
+
+
