c176b18cb2d8b35509278af8165742a3cb5feb4a
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-12-10 16:09:48 +0330

    Change project name

--- chatbothub/chatbothub/asgi.py
+++ chatbothub/chatbothub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for torob_chatbot project.
+ASGI config for chatbothub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'torob_chatbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbothub.settings')
 
 application = get_asgi_application()
--- chatbothub/chatbothub/wsgi.py
+++ chatbothub/chatbothub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for torob_chatbot project.
+WSGI config for chatbothub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'torob_chatbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbothub.settings')
 
 application = get_wsgi_application()
